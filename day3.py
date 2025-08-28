98fffc9465c45815ded0bef8bb00c9f637036a53
Thomaspuetz <Thomaspuetz3@gmail.com>
2025-08-28 12:41:46 -0600

    Day 3 - User input & Personalized Drink Generator

---
+++ day3.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+print("Hello, " + name + "! You are " + age + " years old.")
+
+print(
+    f"Nice to meet you, {name}! In 5 years, you'll be {int(age)+5} years old.")
+
+# Personalized Cocktail
+favorite_liquor = input("What's your favorite liquor? ")
+favorite_mixer = input("What's your favourite mixer? ")
+print(f"Your special drink is: 2 oz {favorite_liquor} + 3 oz {favorite_mixer}")
