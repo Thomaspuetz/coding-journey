f255bf2567b6a38051967ea4546fd1fda13cff59
Thomaspuetz <Thomaspuetz3@gmail.com>
2025-08-28 11:16:52 -0600

    Day 2 - Variables, Math, Margarita Mini-Project

---
+++ day2.py
@@ -0,0 +1,20 @@
+name = "Thomas"
+age = 22
+
+print("Name:", name)
+print("Age:", age)
+
+a = 7
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+tequila = 1.5
+triple_sec = 0.5
+lime_juice = 2
+simple_syrup = 1
+
+total_ounces = tequila + triple_sec + lime_juice + simple_syrup
+print("Total ounces in your Margarita:", total_ounces)
