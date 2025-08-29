87bcfd7a37db48e5064dcf4efd089b31963b878d
Thomaspuetz <Thomaspuetz3@gmail.com>
2025-08-29 09:30:32 -0600

    Day 4 - Cocktail Party Planner

---
+++ day4.py
@@ -0,0 +1,30 @@
+# cocktails = []
+
+# while True:
+import random
+# drink = input("Enter a cocktail (or type 'done' to finish): ")
+# if drink.lower() == "done":
+# break
+# cocktails.append(drink)
+
+# if cocktails:
+# print(f"Your cocktail lineup tonight is: {', '.join(cocktails)}")
+# else:
+# print("You didn't add any cocktails!")
+
+
+cocktails = []
+
+while True:
+    drink = input("Enter a cocktail (or type 'done' to finish):")
+    if drink.lower() == "done":
+        break
+    cocktails.append(drink)
+
+num_guests = int(input("How many people are coming? "))
+
+random.shuffle(cocktails)
+
+for i in range(num_guests):
+    cocktail = cocktails[i % len(cocktails)]
+    print(f"Guest #{i+1} gets a {cocktail}")
